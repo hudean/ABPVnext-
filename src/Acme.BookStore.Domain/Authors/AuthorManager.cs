b159dc189bfcd2edf0ce86addb910d1971db992f
胡德安 <1111@qq.com>
2021-04-14 16:09:14 +0800

    添加项目文件。

---
+++ src/Acme.BookStore.Domain/Authors/AuthorManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Volo.Abp;
+using Volo.Abp.Domain.Services;
+
+namespace Acme.BookStore.Authors
+{
+    //DDD技巧：除非确实需要域服务方法并执行一些核心业务规则，否则请不要引入域服务方法。对于这种情况，我们需要此服务能够强制使用唯一名称约束。
+    public class AuthorManager : DomainService
+    {
+        private readonly IAuthorRepository _authorRepository;
+
+        public AuthorManager(IAuthorRepository authorRepository)
+        {
+            _authorRepository = authorRepository;
+        }
+
+        public async Task<Author> CreateAsync(
+            [NotNull] string name,
+            DateTime birthDate,
+            [CanBeNull] string shortBio = null)
+        {
+            Check.NotNullOrWhiteSpace(name, nameof(name));
+
+            var existingAuthor = await _authorRepository.FindByNameAsync(name);
+            if (existingAuthor != null)
+            {
+                throw new AuthorAlreadyExistsException(name);
+            }
+
+            return new Author(
+                GuidGenerator.Create(),
+                name,
+                birthDate,
+                shortBio
+            );
+        }
+
+        public async Task ChangeNameAsync(
+            [NotNull] Author author,
+            [NotNull] string newName)
+        {
+            Check.NotNull(author, nameof(author));
+            Check.NotNullOrWhiteSpace(newName, nameof(newName));
+
+            var existingAuthor = await _authorRepository.FindByNameAsync(newName);
+            if (existingAuthor != null && existingAuthor.Id != author.Id)
+            {
+                throw new AuthorAlreadyExistsException(newName);
+            }
+
+            author.ChangeName(newName);
+        }
+    }
+}
+
---
+++ test/Acme.BookStore.Application.Tests/Books/BookAppService_Tests.cs
@@ -0,0 +1,76 @@
+﻿using Shouldly;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Validation;
+using Xunit;
+
+namespace Acme.BookStore.Books
+{
+    public class BookAppService_Tests : BookStoreApplicationTestBase
+    {
+        private readonly IBookAppService _bookAppService;
+
+        public BookAppService_Tests()
+        {
+            _bookAppService = GetRequiredService<IBookAppService>();
+        }
+        //Should_Get_List_Of_Books测试只是使用BookAppService.GetListAsync方法来获取和检查书籍清单。
+        //我们可以安全地检查“ 1984”这本书的名称，因为我们知道这本书已经存在于数据库中，因为我们已经将其添加到种子数据中。
+        [Fact]
+        public async Task Should_Get_List_Of_Books()
+        {
+            //Act
+            var result = await _bookAppService.GetListAsync(
+                new PagedAndSortedResultRequestDto()
+            );
+
+            //Assert
+            result.TotalCount.ShouldBeGreaterThan(0);
+            result.Items.ShouldContain(b => b.Name == "1984");
+        }
+
+
+
+        [Fact]
+        public async Task Should_Create_A_Valid_Book()
+        {
+            //Act
+            var result = await _bookAppService.CreateAsync(
+                new CreateUpdateBookDto
+                {
+                    Name = "New test book 42",
+                    Price = 10,
+                    PublishDate = DateTime.Now,
+                    Type = BookType.ScienceFiction
+                }
+            );
+
+            //Assert
+            result.Id.ShouldNotBe(Guid.Empty);
+            result.Name.ShouldBe("New test book 42");
+        }
+
+
+        [Fact]
+        public async Task Should_Not_Create_A_Book_Without_Name()
+        {
+            var exception = await Assert.ThrowsAsync<AbpValidationException>(async () =>
+            {
+                await _bookAppService.CreateAsync(
+                    new CreateUpdateBookDto
+                    {
+                        Name = "",
+                        Price = 10,
+                        PublishDate = DateTime.Now,
+                        Type = BookType.ScienceFiction
+                    }
+                );
+            });
+
+            exception.ValidationErrors
+                .ShouldContain(err => err.MemberNames.Any(mem => mem == "Name"));
+        }
+    }
+}
